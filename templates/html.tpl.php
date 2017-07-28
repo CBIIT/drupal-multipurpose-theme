a3cede5263c65c2f9e88f8f1c7c27aa231bf2c95
Lee Hodge <Lee@LeeHodge.com>
2017-07-28 16:09:37 -0400

    removed DAP tags from master branch

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 <head>
-<script src="//assets.adobedtm.com/f1bfa9f7170c81b1a9a9ecdcc6c5215ee0b03c84/satelliteLib-4b219b82c4737db0e1797b6c511cf10c802c95cb.js"></script>
 <?php print $head; ?>
 <title><?php print $head_title; ?></title>
 <?php print $styles; ?>
@@ -15,6 +14,5 @@
 	<?php print $page_top; ?>
 	<?php print $page; ?>
 	<?php print $page_bottom; ?>
-	<script type="text/javascript">_satellite.pageBottom();</script>
 </body>
 </html>
